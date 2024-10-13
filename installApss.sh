6a5dedafec1af89318330b76c4966250fe66af8f
Sutisna Utamasjida Septiawan <sutisnautamasjida@gmail.com>
2024-10-14 01:07:32 +0700

    update installApss.sh

--- installApss.sh
+++ installApss.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
+flatpak install com.boxy_svg.BoxySVG \
+    com.brave.Browser \ 
+    com.github.PintaProject.Pinta \
+    com.icons8.Lunacy \
+    com.mattjakeman.ExtensionManager \ 
+    io.github.vikdevelop.SaveDesktop \
+    io.missioncenter.MissionCenter \
+    net.devolutions.RDM nl.hjdskes.gcolor3 \
+    org.jaspstats.JASP \
+    -y
 
-flatpak install flathub com.github.finefindus.eyedropper -y
-flatpak install flathub io.missioncenter.MissionCenter -y
-flatpak install flathub org.gnome.Epiphany -y
-flatpak install flathub org.gnome.Extensions -y
-flatpak install flathub nl.hjdskes.gcolor3 -y
-flatpak install flathub com.github.PintaProject.Pinta -y
-flatpak install flathub com.brave.Browser -y
-flatpak install flathub com.sublimetext.three -y
-flatpak install flathub com.mattjakeman.ExtensionManager -y
-flatpak install flathub app.devsuite.Manuals -y
-flatpak install flathub com.boxy_svg.BoxySVG -y
-flatpak install flathub io.github.vikdevelop.SaveDesktop -y
-flatpak install flathub org.jaspstats.JASP -y
-flatpak install flathub net.devolutions.RDM -y
-flatpak install flathub com.icons8.Lunacy -y
-flatpak install flathub dev.lapce.lapce -y
-flatpak install flathub org.kde.yakuake -y
+sudo apt remove gnome-system-monitor -y
